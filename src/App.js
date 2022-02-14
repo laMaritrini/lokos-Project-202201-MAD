ddd795c40b51e3098df2a1a6708e927119443a91
Silvia Ferrai <silviafer@hotmail.it>
2022-02-14 12:44:05 +0100

    Added footer file and installed fontawesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 
+import { Footer } from './components/core/Footer';
+
 export function App() {
-    return <div className="app" />;
+    return (
+        <div className="app">
+            <Footer />
+        </div>
+    );
 }
