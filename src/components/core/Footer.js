348ddf9228ab5fab2a2601e3a7f1fed7f5c58c95
Silvia Ferrai <silviafer@hotmail.it>
2022-02-14 18:28:46 +0100

    Fixed lowercase footer

--- src/components/core/Footer.js
+++ src/components/core/Footer.js
@@ -4,7 +4,7 @@ import {
     faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Footer.scss';
+import './footer.scss';
 
 export function Footer() {
     return (
