d4747864f1cbb1314b9887a50c9bb81ec0b332fb
Silvia Ferrai <silviafer@hotmail.it>
2022-02-14 18:15:18 +0100

    Added pages components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/core/Header';
 import { Menu } from './components/core/Menu';
 import { Footer } from './components/core/Footer';
-import { Route, Routes } from 'react-router';
+import { Home } from './components/Home/Home';
+import { Favorites } from './components/Favorites/Favorites';
+import { Details } from './components/Details/Details';
+import { AboutUs } from './components/AboutUs/AboutUs';
 
 export function App() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -14,12 +18,12 @@ export function App() {
             <Header menuOpen={menuOpen} toggleMenu={toggleMenu} />
             {menuOpen && <Menu />}
             <main>
-            <Routes>
-            <Route path="/" element={}></Route>
-            <Route path="/favorites" element={}></Route>
-            <Route path="/detail/:id" element={}></Route>
-            <Route path="/about-us" element={}></Route>
-            </Routes>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                    <Route path="/detail/:id" element={<Details />} />
+                    <Route path="/about-us" element={<AboutUs />} />
+                </Routes>
             </main>
             <Footer />
         </div>
---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function AboutUs() {
+    return <div>AboutUs</div>;
+}
