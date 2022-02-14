bd9e4df23a9d5f780eea445d06724fbd7f0d9b57
Silvia Ferrai <silviafer@hotmail.it>
2022-02-14 18:02:37 +0100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import './App.css';
 import { Header } from './components/core/Header';
 import { Menu } from './components/core/Menu';
-
 import { Footer } from './components/core/Footer';
+import { Route, Routes } from 'react-router';
 
 export function App() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -14,7 +13,14 @@ export function App() {
         <div className="app">
             <Header menuOpen={menuOpen} toggleMenu={toggleMenu} />
             {menuOpen && <Menu />}
-
+            <main>
+            <Routes>
+            <Route path="/" element={}></Route>
+            <Route path="/favorites" element={}></Route>
+            <Route path="/detail/:id" element={}></Route>
+            <Route path="/about-us" element={}></Route>
+            </Routes>
+            </main>
             <Footer />
         </div>
     );
