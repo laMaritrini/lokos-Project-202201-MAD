62c77588ea836e4616719050d9f342203be8c95c
Heber Baraybar <heber.bar@gmail.com>
2022-02-15 17:50:25 +0100

    Tested Photos

--- src/components/Photo.spec.js
+++ src/components/Photo.spec.js
@@ -1,2 +1,27 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Photo } from './Photo/Photo';
+
+const mockPhoto = {
+    urls: {
+        small: 'Fake photo',
+    },
+    alt_description: 'Alt_description',
+    user: {
+        profile_image: {
+            small: 'Small',
+        },
+        first_name: 'Heber',
+        username: 'hebeready',
+    },
+};
+
+describe('Given photo component', () => {
+    describe('When passed a photo obj', () => {
+        test('It should render', () => {
+            render(<Photo photo={mockPhoto} />);
+            expect(screen.getByAltText(/Alt_description/)).toBeInTheDocument();
+            expect(screen.getAllByText(/hebeready/)).toHaveLength(2);
+        });
+    });
+});
