7659250cb7d5423f0a22e4559d4389a61ff3ed6e
Heber Baraybar <heber.bar@gmail.com>
2022-02-17 18:14:06 +0100

    Tested 100% reducer

--- src/reducers/reducer.spec.js
+++ src/reducers/reducer.spec.js
@@ -18,11 +18,88 @@ const resultState = {
     ],
     favoritePhotos: [],
 };
+const resultLoadState = {
+    photos: [],
+    favoritePhotos: [
+        {
+            id: 1,
+        },
+    ],
+};
 
-describe('photosReducer', () => {
+describe('Given load', () => {
     it('Should return initial state', () => {
         expect(
             photosReducer(stateGiven, actions.loadPhotos(photoList))
         ).toEqual(resultState);
+    });
+});
+describe('Given loadFavorites', () => {
+    it('Should return initial state', () => {
+        expect(
+            photosReducer(stateGiven, actions.loadFavoritePhotos(photoList))
+        ).toEqual(resultLoadState);
+    });
+});
+describe('Given add', () => {
+    it('Should return initial state', () => {
+        expect(photosReducer(stateGiven, actions.addPhotos(photoList))).toEqual(
+            resultLoadState
+        );
+    });
+});
+describe('Given remove', () => {
+    it('Should return initial state', () => {
+        const initialState = {
+            photos: [],
+            favoritePhotos: [
+                {
+                    id: 1,
+                },
+            ],
+        };
+        expect(
+            photosReducer(
+                initialState,
+                actions.removePhoto({
+                    id: 1,
+                })
+            )
+        ).toEqual(stateGiven);
+    });
+});
+
+describe('Given update', () => {
+    it('Should return initial state', () => {
+        const initialState = {
+            photos: [],
+            favoritePhotos: [
+                {
+                    id: 1,
+                },
+            ],
+        };
+        expect(
+            photosReducer(
+                initialState,
+                actions.updateComment({
+                    id: 1,
+                })
+            )
+        ).toEqual(resultLoadState);
+    });
+});
+
+describe('Given default', () => {
+    it('Should return initial state', () => {
+        const initialState = {
+            photos: [],
+            favoritePhotos: [
+                {
+                    id: 1,
+                },
+            ],
+        };
+        expect(photosReducer(initialState, 'heber')).toEqual(initialState);
     });
 });
